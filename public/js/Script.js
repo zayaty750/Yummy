9e4deec338b8a59ba3e147e446dd252c3ad1804f
zeyad <zeyad2103469@miuegypt.edu.eg>
2023-06-20 16:49:32 +0200

    home

--- public/js/Script.js
+++ public/js/Script.js
@@ -9,3 +9,16 @@ function close_Signup()
     document.body.style.overflow = "auto";
     document.getElementById('modalSignin').style.display = "none";
 }
+
+
+function open_Login()
+{
+    document.body.style.overflow = "hidden";
+    document.getElementById('modalLogin').style.display = "block";
+}
+
+function close_Login()
+{
+    document.body.style.overflow = "auto";
+    document.getElementById('modalLogin').style.display = "none";
+}
